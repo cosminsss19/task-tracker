10523501cdc26744191e02738cba92ea8ed79069
cosminsss19 <cosminsss.alex19@gmail.com>
2025-06-28 18:07:28 +0300

    uploaded project

---
+++ task-tracker/src/app/add-task/add-task.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TaskService } from '../services/task.service';
+import { Status } from '../../helpers/help';
+import { Task } from '../task';
+import { NotificationService } from '../notification.service';
+
+@Component({
+  selector: 'app-add-task',
+  standalone: true,
+  imports: [FormsModule],
+  providers: [TaskService],
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.scss'],
+})
+export class AddTaskComponent {
+  taskName: string;
+  taskDescription: string;
+
+  constructor(
+    private router: Router,
+    private taskService: TaskService,
+    private notificationService: NotificationService
+  ) {}
+
+  onSubmit(): void {
+
+    const newTask = <Task>{
+      title: this.taskName,
+      description: this.taskDescription,
+      status: Status.ToDo,
+      assignedTo: 'no one',
+    };
+
+    this.taskService.addTask(newTask).subscribe(() => {
+      this.notificationService.sendMessage('BroadcastMessage', [newTask]);
+      this.router.navigate(['/']);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/']);
+  }
+}
